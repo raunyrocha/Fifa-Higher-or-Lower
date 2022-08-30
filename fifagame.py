68ebdad77da5a35099eb58267097a6d53626740f
Rauny Rocha <r.salles.rocha@gmail.com>
2022-08-30 11:24:57 -0300

    fix the code.

--- fifagame.py
+++ fifagame.py
@@ -3,12 +3,10 @@ import random
 
 with open('players.json', 'r', encoding="utf-8") as b:
     playerdata = json.load(b)
-    for player in playerdata:
-        print("{} is a {} who plays for {} . He has a overall of {}.".format(player["short_name"], player["player_positions"], player["club_name"], player["overall"]))
 
 def higherorlower():
-    A = random.choice(gamerdata)
-    B = random.choice(gamerdata)
+    A = random.choice(playerdata)
+    B = random.choice(playerdata)
     question = "Who has the better overall: A {} or B {}".format(A["short_name"], B["short_name"])
     print(question)
     userchoice = input("Choose A or B: ").upper()

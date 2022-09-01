4cc7cb6a69b8c8b2ac54ebb77cbe8bc64046cd10
Rauny Rocha <r.salles.rocha@gmail.com>
2022-09-01 11:41:51 -0300

    new code deployed.

--- fifagame.py
+++ fifagame.py
@@ -1,39 +1,23 @@
 import json
 import random
 
-with open('players.json', 'r', encoding="utf-8") as b:
-    playerdata = json.load(b)
+with open('players.json', 'r', encoding="utf-8") as data:
+    playerdata = json.load(data)
 
-winnerexpressions = ["Bingo", "Good one", "On the spot", "You're right"]
-loserexpressions = ["Wroooong", "You suck at this", "Nice try", "Own goal"]
+winnerex = ["Bingo", "Good one", "On the spot", "You're right"]
+loserex = ["Wroooong", "You suck at this", "Nice try", "Own goal"]
 
-def higherorlower():
+winnermsg = random.choice(winnerex)
+losermsg = random.choice(loserex)
+
+list_continue = []
+
+def initiallist():
     A = random.choice(playerdata)
     B = random.choice(playerdata)
-    ahigher = "{} has the higher overall with {}.".format(A["short_name"], A["overall"])
-    bhigher = "{} has the higher overall with {}.".format(B["short_name"], B["overall"])
-    question = "Who has the higher overall on FIFA 22: A - {}({} from {}) or B - {}({} from {})".format(A["short_name"], A["player_positions"], A["club_name"], B["short_name"], B["player_positions"], B["club_name"])
-    print(question)
-    userchoice = input("Choose A or B: ").upper()
-    while userchoice == "A":
-        if A["overall"] > B["overall"]:
-            print(random.choice(winnerexpressions))
-            print(ahigher)
-            break
-        else:
-            print(random.choice(loserexpressions))
-            print(bhigher)
-            break
-
-    while userchoice == "B":
-        if A["overall"] > B["overall"]:
-            print(random.choice(loserexpressions))
-            print(ahigher)
-            break
-        else:
-            print(random.choice(winnerexpressions))
-            print(bhigher)
-            break
+    list_continue.append(A)
+    list_continue.append(B)
 
 
-higherorlower()
+initiallist()
+print(list_continue)

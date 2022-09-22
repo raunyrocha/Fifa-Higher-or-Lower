82da4e7a693c47efa55c19678ddb01749c9d4544
Rauny Rocha <r.salles.rocha@gmail.com>
2022-09-22 17:17:25 -0300

    changed tries to the single json file.

---
+++ fgentries.py
@@ -0,0 +1,30 @@
+from statistics import mean
+import json
+
+totalplays = 0
+totalpl = 0
+
+with open("scoreboard.json", "r") as tries1:
+    dict = json.load(tries1)
+    newtries = []
+    for item in dict:
+        totalpl +=1
+        newtries.append(int(item["Score"]))
+    print(newtries)
+
+
+with open("tries.txt", "r") as tries:
+    listtries = tries.readlines()
+    newlist = []
+    for x in listtries:
+        totalplays += 1
+        newlist.append(int(x.replace("\n", "")))
+
+def Average(l):
+    avg = mean(l)
+    return avg
+
+Alltries = sum(newlist)
+
+averagetries = Average(newlist)
+avgtries = round(averagetries, 2)
